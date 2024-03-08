be9fdf12f8dba7a571d1eff84ed63b7cc2a33de1
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2024-03-08 11:26:13 +0530

    steps 2-7

--- webapp/Index.js
+++ webapp/Index.js
@@ -1,18 +1,47 @@
 sap.ui.require([
-	"sap/m/Text",
-    "sap/ui/model/json/JSONModel"
-], function (Text,JSONModel) {
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/mvc/XMLView",
+	"sap/ui/model/resource/ResourceModel"
+], function (JSONModel, XMLView, ResourceModel) {
 	"use strict";
 
 	// Attach an anonymous function to the SAPUI5 'init' event
 	sap.ui.getCore().attachInit(function () {
-		// Create a text UI element that displays a hardcoded text string
-        var oModel = new JSONModel({
-			greetingText: "Hi,, my name is Harry Hawk"
+		// Create a JSON model from an object literal
+		var oModel = new JSONModel({
+			firstName: "Harry",
+			lastName: "Hawk",
+			enabled: true,
+			address: {
+				street: "Dietmar-Hopp-Allee 16",
+				city: "Walldorf",
+				zip: "69190",
+				country: "Germany"
+			}
 		});
 
 		// Assign the model object to the SAPUI5 core
 		sap.ui.getCore().setModel(oModel);
-		new Text({text: "{/greetingText}"}).placeAt("content");
+
+		// Create a resource bundle for language specific texts
+		// the configured supportedLocales represent the i18n files present:
+		// * "" - i18n/i18n.properties
+		// the configured fallbackLocale should represent one of these files
+		// * "" - according to the fallback chain the root bundle is the last fallback.
+		//   Configuring it explicitly avoids side effects when additional resource files are added.
+		// @see https://ui5.sap.com/#/topic/ec753bc539d748f689e3ac814e129563
+		var oResourceModel = new ResourceModel({
+			bundleName: "sap.ui.demo.db.i18n.i18n",
+			supportedLocales: ["", "de"],
+			fallbackLocale: ""
+		});
+
+		// Assign the model object to the SAPUI5 core using the name "i18n"
+		sap.ui.getCore().setModel(oResourceModel, "i18n");
+
+		// Display the XML view called "App"
+		new XMLView({
+			viewName: "sap.ui.demo.db.view.App"
+		}).placeAt("content");
 	});
 });
