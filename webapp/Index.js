22bc8fbd8918e620ad1ad48f16b09a41b242475b
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2024-03-13 11:00:26 +0530

    complete

--- webapp/Index.js
+++ webapp/Index.js
@@ -1,26 +1,31 @@
 sap.ui.require([
+	"sap/ui/core/Core",
+	"sap/ui/core/Messaging",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/mvc/XMLView",
 	"sap/ui/model/resource/ResourceModel"
-], function (JSONModel, XMLView, ResourceModel) {
+], function (Core, Messaging, JSONModel, XMLView, ResourceModel) {
 	"use strict";
 
-	// Attach an anonymous function to the SAPUI5 'init' event
-	sap.ui.getCore().attachInit(function () {
-		// Create a JSON model from an object literal
+	// Chain an anonymous function to the SAPUI5 'ready' Promise
+	Core.ready().then(function () {
+		var oProductModel = new JSONModel();
+		oProductModel.loadData("./model/Products.json");
+		sap.ui.getCore().setModel(oProductModel, "products");
+
 		var oModel = new JSONModel({
 			firstName: "Harry",
 			lastName: "Hawk",
 			enabled: true,
-            panelHeaderText: "Data Binding Basics",
-            address: {
+			address: {
 				street: "Dietmar-Hopp-Allee 16",
 				city: "Walldorf",
 				zip: "69190",
 				country: "Germany"
 			},
-            salesAmount:12345.654,
-            currencyCode:"BTC"
+			salesAmount: 12345.6789,
+            priceThreshold: 20,
+			currencyCode: "EUR"
 		});
 
 		// Assign the model object to the SAPUI5 core
@@ -28,16 +33,21 @@ sap.ui.require([
 
 		var oResourceModel = new ResourceModel({
 			bundleName: "sap.ui.demo.db.i18n.i18n",
-			supportedLocales: ["", "de"],
-			fallbackLocale: ""
+			fallbackLocale: "",
+			supportedLocales: ["", "de"]
 		});
 
-		// Assign the model object to the SAPUI5 core using the name "i18n"
 		sap.ui.getCore().setModel(oResourceModel, "i18n");
 
 		// Display the XML view called "App"
-		new XMLView({
+		var oView = new XMLView({
 			viewName: "sap.ui.demo.db.view.App"
-		}).placeAt("content");
+		});
+
+		// Register the view with the message manager
+		Messaging.registerObject(oView, true);
+
+		// Insert the view into the DOM
+		oView.placeAt("content");
 	});
 });
